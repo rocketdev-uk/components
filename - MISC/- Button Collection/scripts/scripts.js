243527bdf4683864ed7e05cf79cfc2f10aaa4c75
rocketdev-uk <mykee@rocketdev.co.uk>
2024-09-17 16:19:22 +0100

    Add Button Collection
    Add Button Collection

---
+++ - MISC/- Button Collection/scripts/scripts.js
@@ -0,0 +1,4 @@
+// Add event listener to any button by changing the target class below for example.....
+document.querySelector('.button-neon').addEventListener('click', function() {
+  alert('You clicked the neon button!');
+});
